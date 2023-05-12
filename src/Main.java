ce3debecc56732935bc70266def8210a8c8527d7
ArtemSh <artem_shalaev00@mail.ru>
2023-05-12 09:21:00 +0300

    ver 1.0 First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Box box1 = new Box();
+
+    box1.Length = 10.0;
+    box1.Width = 5.0;
+    box1.Height = 2.0;
+
+    double result;
+
+    result = box1.Length* box1.Width* box1.Height;
+
+    System.out.println(result);
     }
 }

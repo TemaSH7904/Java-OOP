c8064ac6a6bf064a2009d2cca54ec867c008f3b9
ArtemSh <artem_shalaev00@mail.ru>
2023-05-13 09:52:06 +0300

    13.05.2023 Добавление двух классов из темы Введение в методы и параметризированные методы

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+// Параметризивроаные методы - методы в которые передаются или принимают параметры (числа, строки)
+public class Test {
+
+    int square(){
+        return 10 * 10;
+    }
+
+    int number(int num){
+        return num * num;
+    }
+}
